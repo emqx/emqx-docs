a7d5c3158617de219d01c71277442adc5985cb7f
Feng Lee <feng@emqtt.io>
2017-09-12 23:56:02 +0800

    Update company name

--- source/conf.py
+++ source/conf.py
@@ -51,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'EMQ - 百万级开源MQTT消息服务器'
-copyright = u'2016-2017, 李枫, 杭州小莉科技有限公司'
+copyright = u'2016-2017, 李枫, 杭州映云科技有限公司'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -231,7 +231,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   ('index', 'EMQ.tex', u'EMQ - 百万级开源MQTT消息服务器',
-   u'杭州小莉科技有限公司 <contact@emqtt.io>', 'manual'),
+   u'杭州映云科技有限公司 <contact@emqtt.io>', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -275,7 +275,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'EMQ', u'EMQ Documentation',
-   u'杭州小莉科技有限公司 <contact@emqtt.io>', 'EMQ', 'One line description of project.',
+   u'杭州映云科技有限公司 <contact@emqtt.io>', 'EMQ', 'One line description of project.',
    'Miscellaneous'),
 ]
 
