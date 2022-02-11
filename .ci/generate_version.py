86ec17f82a1c89df36bcc872d5337664995a8051
Swilder-M <poxiaobbs@gmail.com>
2022-02-11 11:41:43 +0800

    fix(ci): fix deploy action

--- .ci/generate_version.py
+++ .ci/generate_version.py
@@ -1,11 +1,10 @@
 import sys
 import json
 
-version_list = sys.argv[1]
+version_list = sys.argv[1:]
 
 if __name__ == '__main__':
-    version_list = version_list.replace('e', 'v').split(' ')
-    version_list = list(map(lambda v: v.split('-')[0], version_list))
+    version_list = list(map(lambda v: v.split('-')[0].replace('e', 'v'), version_list))
     version_list = list(set(version_list))
     version_list.sort(
         key=lambda v: [int(u) for u in v.replace('v', '').split('.')],
