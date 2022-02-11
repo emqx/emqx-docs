8897875429cd68cefaeabb2e3c51a745ed0958b1
Swilder-M <poxiaobbs@gmail.com>
2022-02-11 09:27:21 +0800

    fix(ci): set version list

--- .ci/generate_version.py
+++ .ci/generate_version.py
@@ -6,6 +6,7 @@ version_list = sys.argv[1]
 if __name__ == '__main__':
     version_list = version_list.replace('e', 'v').split(' ')
     version_list = list(map(lambda v: v.split('-')[0], version_list))
+    version_list = list(set(version_list))
     version_list.sort(
         key=lambda v: [int(u) for u in v.replace('v', '').split('.')],
         reverse=True
