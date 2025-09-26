e2de00937b46ac7404e9829b64c63d414475e976
Ivan Dyachkov <dev@dyachkov.org>
2025-09-26 09:22:29 +0200

    ci: fix update-changelog.sh script

--- update-changelog.sh
+++ update-changelog.sh
@@ -33,13 +33,8 @@ process_changes() {
   for f in $DEV_CHANGES_DIR/$source_edition/fix-*.md $DEV_CHANGES_DIR/$source_edition/feat-*.md; do
     pr_num="$(echo "${f}" | sed -E 's/.*-([0-9]+)\.[a-z]+\.md$/\1/')"
     if ! grep -q "^- \[#$pr_num\]" en_US/changes/changes-$target_edition-v$major.md; then
-      if [ $pr_num -lt 10000 ]; then
-        repo='emqx-platform'
-      else
-        repo='emqx'
-      fi
       {
-        echo "- [#${pr_num}](https://github.com/emqx/$repo/pull/${pr_num}) $(head -n 1 "$f")"
+        echo "- [#${pr_num}](https://github.com/emqx/emqx/pull/${pr_num}) $(head -n 1 "$f")"
         # indent the content
         tail -n +2 "$f" | sed '/^$/!s/^/  /'
         echo ""
@@ -62,35 +57,32 @@ process_changes() {
   # exit the function if no breaking changes files are found
   [ $num_files -eq 0 ] && return
 
+  breaking_changes="en_US/changes/breaking-changes-$major_minor.md"
+
   # create en_US/changes/breaking-changes-$source_edition-$major_minor.md if it does not exist
-  if [ ! -f en_US/changes/breaking-changes-$target_edition-$major_minor.md ]; then
-    echo "# Incompatible Changes in EMQX $major_minor" > en_US/changes/breaking-changes-$target_edition-$major_minor.md
-    echo "" >> en_US/changes/breaking-changes-$target_edition-$major_minor.md
-    echo "" >> en_US/changes/breaking-changes-$target_edition-$major_minor.md
+  if [ ! -f "${breaking_changes}" ]; then
+    echo "# Incompatible Changes in EMQX $major_minor" > "${breaking_changes}"
+    echo "" >> "${breaking_changes}"
+    echo "" >> "${breaking_changes}"
   fi
 
   # Add a section for the new version in breaking-changes if it doesn't exist
-  if ! grep -q "^## $EMQX_VERSION" en_US/changes/breaking-changes-$target_edition-$major_minor.md; then
+  if ! grep -q "^## $EMQX_VERSION" "${breaking_changes}"; then
     version=$( [[ $target_edition = ce ]] && echo "v$EMQX_VERSION" || echo "e$EMQX_VERSION" )
-    sed -i "3i ## $version\n" en_US/changes/breaking-changes-$target_edition-$major_minor.md
+    sed -i "3i ## $version\n" "${breaking_changes}"
   fi
 
   for f in $DEV_CHANGES_DIR/$source_edition/breaking-*.md; do
     pr_num="$(echo "${f}" | sed -E 's/.*-([0-9]+)\.[a-z]+\.md$/\1/')"
-    if ! grep -q "^- \[#$pr_num\]" en_US/changes/breaking-changes-$target_edition-$major_minor.md; then
-      if [ $pr_num -lt 10000 ]; then
-        repo='emqx-platform'
-      else
-        repo='emqx'
-      fi
+    if ! grep -q "^- \[#$pr_num\]" "${breaking_changes}"; then
       {
-        echo "- [#${pr_num}](https://github.com/emqx/$repo/pull/${pr_num}) $(head -n 1 "$f")"
+        echo "- [#${pr_num}](https://github.com/emqx/emqx/pull/${pr_num}) $(head -n 1 "$f")"
         # indent the content
         tail -n +2 "$f" | sed '/^$/!s/^/  /'
         echo ""
       } > /tmp/$pr_num.md
 
-      sed -i "4r /tmp/$pr_num.md" en_US/changes/breaking-changes-$target_edition-$major_minor.md
+      sed -i "4r /tmp/$pr_num.md" "${breaking_changes}"
     fi
   done
 }
